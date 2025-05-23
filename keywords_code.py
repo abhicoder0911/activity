f648cab058570e469071a38fd24e19e3035cc54c
abhicoder0911 <siri.munni@gmail.com>
2025-05-23 20:16:08 +0530

    Add files via upload

---
+++ keywords_code.py
@@ -0,0 +1,22 @@
+'''var=10
+while var>0:
+    var=var-1
+    if var==5:
+        continue
+     #display result
+    print('\nCurrent variable value :', var)
+print("\nGood bye!")'''
+
+a=input("enter a word: ")
+#program to check break keyword
+for i in a: # itrate loop
+    if(i=='A'): # condition 1
+        # display result
+        print("A is found ")
+        break # break statement
+    else:
+        # display result
+        #print("A not found")
+        pass
+    
+    
