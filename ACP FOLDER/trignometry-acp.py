10651fb38306be01e0a7ce1a74038eb9d94412b0
abhicoder0911 <siri.munni@gmail.com>
2025-05-25 22:15:55 +0530

    Add files via upload

---
+++ ACP FOLDER/trignometry-acp.py
@@ -0,0 +1,10 @@
+import math as m
+a=int(input("enter an angle in degrees  "))
+rad=m.radians(a)
+sin_value=m.sin(rad)
+
+cos_value=m.cos(rad)
+tan_value=m.tan(rad)
+print(f"sin value {sin_value}")
+print(f"cos value {cos_value}")
+print(f"tan value {tan_value}")
