3293e6e6cc1012f215aa4996666ff2a33bac531d
abhicoder0911 <siri.munni@gmail.com>
2025-06-07 10:54:26 +0530

    Add files via upload

---
+++ ACP FOLDER/sym func .py
@@ -0,0 +1,10 @@
+# using symmetric difference
+set1={1,2,3,4,5,6,7,8,9}
+set2={7,8,9,11,12,13,14}
+set0=set1.symmetric_difference(set2)
+print(set1)
+
+set3={'red','green','black'}
+set4={'pink','blue','black','green'}
+set01=set3.symmetric_difference(set4)
+print(set01)
