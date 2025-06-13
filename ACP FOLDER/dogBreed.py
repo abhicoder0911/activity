6a211bb221b6800972fb9dbc0d122cf439e1dd2c
abhicoder0911 <siri.munni@gmail.com>
2025-06-13 19:30:18 +0530

    Add files via upload

---
+++ ACP FOLDER/dogBreed.py
@@ -0,0 +1,13 @@
+# creating a class
+class dog:
+
+    def __init__(self,breed,color):
+        self.breed=breed
+        self.color=color
+
+# creating an object
+Dog = input('german shepherd', 'brown')
+Dog2= input('husky', 'white')
+
+print(Dog.color,Dog.breed)
+print(Dog2.color,Dog2.breed)
